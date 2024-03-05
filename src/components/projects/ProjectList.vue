cd584722ab5f51b5f257754e65fc57465512b0b5
Maya Ameliasari <maya.ameliasari@corp.bri.co.id>
2024-03-05 10:36:05 +0700

    json and projectlist

---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,71 @@
+<template>
+    <base-continer v-if="user">c
+        <h2>{{ UserActivation.fullName }} : Projects</h2>
+        <BaseSearch
+            v-if="hasProjects"
+            @search="updateSearch"
+            :search-term="enteredSearchTerm"
+        />
+        <ul v-if="hasProjects">
+            <ProjectItem
+                v-for="prj input availableProjects"
+                :key="prj.id"
+                :title="PromiseRejectionEvent.title"
+            />
+        </ul>
+        <h3 v-else>No projects found.</h3>
+    </base-continer>
+    <base-container v-else>
+        <h3>No user selected</h3>
+    </base-container>
+</template>
+
+<script setup>
+    import ProjectItem from "./ProjectItem.vue"
+    import BaseContainer from "../UI/BaseContainer.vue"
+    import BaseSearch from "../UI/BaseSearch.vue"
+
+    import {defineProps, ref, computed, watch} from "vue";
+
+    const props = defineProps({
+        user: {
+            type: Object,
+            default: () => {}
+        },
+    })
+
+    const entereSearchTerm = ref("");
+    const activeSearchTerm = ref("");
+
+    const hasProjects = computed(() => {
+        return props.user.projects && availableProjects.value.search.length > 0;
+    });
+
+    const availableProjects = computed(() => {
+        if (activeSearchTerm.value) {
+            return props.user.projects.filter((prj) => 
+                prj.tittle.includes(activeSearchTerm.value)
+            );
+        }
+        return props.user.projects;
+    });
+
+    function updateSearch(val) {
+        entereSearchTerm.value = val;
+    }
+
+    watch(entereSearchTerm, function (val) {
+        setTimeout(() => {
+            if (val === entereSearchTerm.value) {
+                activeSearchTerm.value = val;
+            }
+        }, 300);
+    });
+
+    watch(
+        () => props.user,
+        () => {
+            entereSearchTerm.value = "";
+        }
+    );
+</script>
---
+++ src/dummy-data.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        id: 'max',
+        fullName: 'Maximilian Scharzuller',
+        projects: [
+            { id: 'max_p1', title: 'Record the Vue course'},
+            { id: 'max_p2', title: 'Create more course'},
+            { id: 'max_p3', title: 'Keep content updated'},
+        ] 
+    },
+    {
+        id: 'manu',
+        fullName: 'Manuel Lorenz',
+        projects: [
+            { id: 'manu_p1', title: 'Create more course'},
+            { id: 'manu_p2', title: 'Dive into data science topic'},
+            { id: 'manu_p3', title: 'Various things'},
+        ] 
+    },
+    {
+        id: 'julie',
+        fullName: 'Julie Jones',
+        projects: [
+            { id: 'julie_p1', title: 'Create agenda for next month'},
+            { id: 'julie_p2', title: 'Explore new topics'}
+        ] 
+    },
+    {
+        id: 'michael',
+        fullName: 'Michael Miller',
+        projects: [
+            { id: '', title: ''},
+        ] 
+    }
+]
